ad00406cc196cc6b584d9f6fb2ac56c0d9011efa
GustasD1 <92514402+GustasD1@users.noreply.github.com>
2022-04-03 08:43:12 -0700

    Add files via upload

---
+++ Studentu Vidurkis Deque/Studentu Vidurkis (Vektoriai)/prototipai.h
@@ -0,0 +1,23 @@
+#ifndef prototipai_H_INCLUDE
+#define prototipai_H_INCLUDE
+
+
+#include "biblioteka.h"
+
+
+
+
+
+void ivestis(data& temp, int& n);
+void isvedimas(const data& temp);
+void apskaiciavimas(data& temp, int n);
+void skaitymas(deque<data>& temp);
+void FailoIsvedimas(deque<data>& temp);
+void generacija(deque<data>& temp);
+
+
+
+
+
+
+#endif
